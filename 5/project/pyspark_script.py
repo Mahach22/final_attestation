84e856c66015cf55cf97a4492146c6e9538955a8
mahach <mahach2211@gmail.com>
2024-11-23 01:23:11 +0300

    обновил решение

--- 5/project/pyspark_script.py
+++ 5/project/pyspark_script.py
@@ -1,6 +1,64 @@
+# from pyspark.sql import SparkSession
+# from clickhouse_driver import Client
+#
+# # Настройки PostgreSQL
+# pg_config = {
+#     "host": "localhost",
+#     "port": 5432,
+#     "user": "user",
+#     "password": "password",
+#     "database": "testdb"
+# }
+#
+# # Настройки ClickHouse
+# ch_config = {
+#     "host": "localhost",
+#     "port": 9000,
+#     "username": "default",
+#     "password": "default"
+# }
+#
+# # Создание SparkSession
+# spark = SparkSession.builder \
+#     .appName("Postgres and ClickHouse Integration") \
+#     .config("spark.jars", "postgresql-42.7.4.jar") \
+#     .getOrCreate()
+#
+# # Чтение данных из PostgreSQL
+# pg_df = spark.read.format("jdbc").options(
+#     url=f"jdbc:postgresql://{pg_config['host']}:{pg_config['port']}/{pg_config['database']}",
+#     dbtable="test_table",  # Название таблицы или SQL-запрос в виде "(SELECT * FROM table) AS alias"
+#     user=pg_config['user'],
+#     password=pg_config['password'],
+#     driver="org.postgresql.Driver"
+# ).load()
+#
+# # Чтение данных из ClickHouse
+# ch_client = Client(
+#     host=ch_config['host'],
+#     port=ch_config['port'],
+#     user=ch_config['username'],
+#     password=ch_config['password']
+# )
+#
+# # Выполнение запроса в ClickHouse и преобразование в Spark DataFrame
+# ch_query = "SELECT * FROM test_table"
+# ch_data = ch_client.query_dataframe(ch_query)
+# ch_spark_df = spark.createDataFrame(ch_data)
+#
+# # Вывод данных из PostgreSQL
+# print("Данные из PostgreSQL:")
+# pg_df.show()
+#
+# # Вывод данных из ClickHouse
+# print("Данные из ClickHouse:")
+# ch_spark_df.show()
+#
+# # Завершение работы SparkSession
+# spark.stop()
+
+
 from pyspark.sql import SparkSession
-import psycopg2
-from clickhouse_driver import Client
 
 # Настройки PostgreSQL
 pg_config = {
@@ -14,40 +72,43 @@ pg_config = {
 # Настройки ClickHouse
 ch_config = {
     "host": "localhost",
-    "port": 9000,
+    "port": 8123,  # HTTP порт ClickHouse для JDBC
     "username": "default",
-    "password": "default"
+    "password": "default",
+    "database": "default"
 }
 
 # Создание SparkSession
 spark = SparkSession.builder \
-    .appName("Postgres and ClickHouse Integration").config("spark.jars", "postgresql-42.7.4.jar") \
+    .appName("Postgres and ClickHouse Integration") \
+    .config("spark.jars", "postgresql-42.7.4.jar,clickhouse-jdbc-0.4.6.jar") \
     .getOrCreate()
 
 # Чтение данных из PostgreSQL
-pg_conn = psycopg2.connect(**pg_config)
-pg_query = "SELECT * FROM test_table"
 pg_df = spark.read.format("jdbc").options(
     url=f"jdbc:postgresql://{pg_config['host']}:{pg_config['port']}/{pg_config['database']}",
-    dbtable="(SELECT * FROM test_table) as temp_table",
+    dbtable="test_table",
     user=pg_config['user'],
     password=pg_config['password'],
     driver="org.postgresql.Driver"
 ).load()
 
 # Чтение данных из ClickHouse
-ch_client = Client(host=ch_config['host'], port=ch_config['port'], user=ch_config['username'], password=ch_config['password'])
-ch_query = "SELECT * FROM test_table"
-ch_data = ch_client.query_dataframe(ch_query)
+ch_df = spark.read.format("jdbc").options(
+    url=f"jdbc:clickhouse://{ch_config['host']}:{ch_config['port']}/{ch_config['database']}",
+    dbtable="test_table",
+    user=ch_config['username'],
+    password=ch_config['password'],
+    driver="com.clickhouse.jdbc.ClickHouseDriver"
+).load()
 
-# Конвертация ClickHouse данных в Spark DataFrame
-ch_df = spark.createDataFrame(ch_data)
-
-#вывод объединённых данных
+# Вывод данных из PostgreSQL
 print("Данные из PostgreSQL:")
 pg_df.show()
+
+# Вывод данных из ClickHouse
 print("Данные из ClickHouse:")
 ch_df.show()
 
-# завершение работы
+# Завершение работы SparkSession
 spark.stop()
